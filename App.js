59c39874f1f1e83fbd5e718765c41ac52930c452
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-03-19 16:36:36 +0530

    config driven ui set up using props and props destructuring also done

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const Title = () => (
     />
   </a>
 );
-const HeaderComponent = () => {
+const Header = () => {
   return (
     <div className="header">
       <Title />
@@ -42,10 +42,539 @@ const HeaderComponent = () => {
     </div>
   );
 };
+const restaurantList = [
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "253735",
+          name: "McDonald's",
+          cloudinaryImageId: "535fc9f9c135f7982317bbb6a64a1ffc",
+          locality: "Supertech Shopprix Mall",
+          areaName: "Sector 61",
+          costForTwo: "₹400 for two",
+          cuisines: ["American"],
+          avgRating: 4.4,
+          parentId: "630",
+          avgRatingString: "4.4",
+          totalRatingsString: "10K+",
+          sla: {
+            deliveryTime: 29,
+            lastMileTravel: 3,
+            serviceability: "SERVICEABLE",
+            slaString: "25-30 mins",
+            lastMileTravelString: "3.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-19 23:45:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          aggregatedDiscountInfoV2: {},
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textExtendedBadges: {},
+              textBased: {},
+              imageBased: {},
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=253735&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "90170",
+          name: "Burger King",
+          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+          locality: "H Block",
+          areaName: "Sector 63",
+          costForTwo: "₹350 for two",
+          cuisines: ["Burgers", "American"],
+          avgRating: 4.3,
+          parentId: "166",
+          avgRatingString: "4.3",
+          totalRatingsString: "10K+",
+          sla: {
+            deliveryTime: 31,
+            lastMileTravel: 3.6,
+            serviceability: "SERVICEABLE",
+            slaString: "30-35 mins",
+            lastMileTravelString: "3.6 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-20 06:00:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "ITEMS",
+            subHeader: "AT ₹139",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=90170&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "663618",
+          name: "Burgerama",
+          cloudinaryImageId: "1d52387b37dbec1712d0142aec3421a6",
+          areaName: "Sector - 73",
+          costForTwo: "₹700 for two",
+          cuisines: ["Burgers", "American", "Beverages", "Desserts"],
+          avgRating: 4.2,
+          parentId: "8993",
+          avgRatingString: "4.2",
+          totalRatingsString: "100+",
+          promoted: true,
+          adTrackingId:
+            "cid=11695106~p=2~eid=0000018e-55b1-b4fc-0501-b90600ec0227~srvts=1710834693372~83637",
+          sla: {
+            deliveryTime: 35,
+            lastMileTravel: 4.7,
+            serviceability: "SERVICEABLE",
+            slaString: "30-35 mins",
+            lastMileTravelString: "4.7 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-20 04:00:00",
+            opened: true,
+          },
+          badges: {
+            imageBadges: [
+              {
+                imageId: "newg.png",
+                description: "Gourmet",
+              },
+            ],
+          },
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {
+                badgeObject: [
+                  {
+                    attributes: {
+                      imageId: "newg.png",
+                      description: "Gourmet",
+                    },
+                  },
+                ],
+              },
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "60% OFF",
+            subHeader: "UPTO ₹120",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "3.7",
+              ratingCount: "3",
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=663618&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "842681",
+          name: "Roll Station",
+          cloudinaryImageId: "d4bfc1c40ff76d935ebad17599d26b0c",
+          locality: "Sector 64",
+          areaName: "Sector 64",
+          costForTwo: "₹300 for two",
+          cuisines: ["rolls", "North Indian", "Fast Food", "Beverages"],
+          avgRating: 4.6,
+          parentId: "170837",
+          avgRatingString: "4.6",
+          totalRatingsString: "10+",
+          sla: {
+            deliveryTime: 27,
+            lastMileTravel: 1.2,
+            serviceability: "SERVICEABLE",
+            slaString: "25-30 mins",
+            lastMileTravelString: "1.2 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-19 23:59:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textExtendedBadges: {},
+              textBased: {},
+              imageBased: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "20% OFF",
+            subHeader: "UPTO ₹50",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          isNewlyOnboarded: true,
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=842681&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "37699",
+          name: "Grill Inn",
+          cloudinaryImageId: "trp47yfx3qa4k0ydgxq8",
+          locality: "A Block",
+          areaName: "Sector 62",
+          costForTwo: "₹350 for two",
+          cuisines: ["Italian"],
+          avgRating: 4.2,
+          parentId: "432",
+          avgRatingString: "4.2",
+          totalRatingsString: "500+",
+          sla: {
+            deliveryTime: 33,
+            lastMileTravel: 3,
+            serviceability: "SERVICEABLE",
+            slaString: "30-35 mins",
+            lastMileTravelString: "3.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-19 21:45:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "15% OFF",
+            subHeader: "UPTO ₹45",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=37699&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "815788",
+          name: "Hulk's Burger",
+          cloudinaryImageId: "8c4a345ed479abc107fab212cbb0a4d6",
+          locality: "Sector 50",
+          areaName: "Sector 50",
+          costForTwo: "₹350 for two",
+          cuisines: ["Burgers", "American"],
+          avgRating: 4.5,
+          parentId: "462215",
+          avgRatingString: "4.5",
+          totalRatingsString: "20+",
+          promoted: true,
+          adTrackingId:
+            "cid=11861979~p=3~eid=0000018e-55b1-b4fc-0501-b90700ec0312~srvts=1710834693372~83637",
+          sla: {
+            deliveryTime: 37,
+            lastMileTravel: 4.7,
+            serviceability: "SERVICEABLE",
+            slaString: "35-40 mins",
+            lastMileTravelString: "4.7 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-03-19 23:00:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "₹125 OFF",
+            subHeader: "ABOVE ₹249",
+            discountTag: "FLAT DEAL",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          isNewlyOnboarded: true,
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=815788&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+];
+// below line we did props destructuring, like some cool developers
+const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+        alt="food image"
+      />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{avgRating} Stars</h4>
+    </div>
+  );
+};
+// Props- properties that we pass as an argument to a functional component
 const Body = () => {
   return (
-    <div className="body">
-      <h1>Body Component</h1>
+    <div className="restaurant-list">
+      <RestaurantCard {...restaurantList[0].card.card.info} />
+      <RestaurantCard {...restaurantList[1].card.card.info} />
+      <RestaurantCard {...restaurantList[2].card.card.info} />
+      <RestaurantCard {...restaurantList[3].card.card.info} />
+      <RestaurantCard {...restaurantList[4].card.card.info} />
+      <RestaurantCard {...restaurantList[5].card.card.info} />
     </div>
   );
 };
@@ -59,7 +588,7 @@ const Footer = () => {
 const AppLayout = () => {
   return (
     <>
-      <HeaderComponent />
+      <Header />
       <Body />
       <Footer />
     </>
