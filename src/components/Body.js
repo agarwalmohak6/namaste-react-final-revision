8cd39ec2ad631af3a72296dc3ff92d3942eb6b9f
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-03-20 11:11:50 +0530

    hooks created and made search bar functional and updated restaurant list according to search on click of search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,52 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
-// Props- properties that we pass as an argument to a functional component
+
+// function to update restaurant list using search functionality
+const filterRestaurant = (searchText, restaurants) => {
+  return restaurants.filter((r) =>
+    r.card?.card?.info?.name.includes(searchText)
+  );
+};
+
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setrestaurants] = useState(restaurantList);
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return (
-          <RestaurantCard
-            {...restaurant?.card?.card?.info}
-            key={restaurant?.card?.card?.info.id}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            // need to filter data
+            const data = filterRestaurant(searchText, restaurants);
+            // update the state
+            setrestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard
+              {...restaurant?.card?.card?.info}
+              key={restaurant?.card?.card?.info.id}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
